07165a720b7a0526514f7a869a3759d7b5960790
JW <anylee2142@gmail.com>
2018-04-15 23:22:57 +0900

    180415 Database Schema, Seeding completed

---
+++ DB/table_schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS User (
+	user_id int auto_increment,
+    id varchar(100) UNIQUE,
+    pw varchar(30),
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS Article (
+	article_id int auto_increment,
+    title varchar(100),
+    content MEDIUMTEXT,
+    area int,
+    written_date datetime,
+    PRIMARY KEY (article_id)
+);
+
+CREATE TABLE IF NOT EXISTS view(
+	user_id int,
+    article_id int,
+    view_date datetime,
+    PRIMARY KEY (user_id, article_id, view_date),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (article_id) REFERENCES Article(article_id)
+);
+
