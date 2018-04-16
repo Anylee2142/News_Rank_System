92a6123ec5a6a72d7e7aedb064e52da7ef8bba43
JW <anylee2142@gmail.com>
2018-04-17 03:12:19 +0900

    180417 News rank

---
+++ naver_news_search.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import urllib.request
+import json
+
+def api_search(keyword):
+    with open('../naver_api_info.txt', 'r') as f:
+        client_id = str(f.readline().replace('\n',''))
+        client_secret = str(f.readline().replace('\n',''))
+
+    encText = urllib.parse.quote(keyword)
+    encDisp = urllib.parse.quote("100")
+    url = "https://openapi.naver.com/v1/search/news?query={}&display={}".format(encText, encDisp)
+    # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", client_id)
+    request.add_header("X-Naver-Client-Secret", client_secret)
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+    if(rescode == 200):
+        response_body = response.read()
+        return json.loads(response_body.decode('utf-8'))
+    else:
+        print('Response without code 200')
+        return None
