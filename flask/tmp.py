0185cf5cdbe709e27a8ec270733bef135ce32a89
Alan Lee <anylee2142@gmail.com>
2021-04-25 23:07:38 -0700

    Updated

--- flask/tmp.py
+++ flask/tmp.py
@@ -5,15 +5,11 @@ app = Flask(__name__)
 @app.route('/files/<path>/<filename>')
 def test(path, filename):
 
-    # response = make_response(send_file('/home/ej/변성윤.txt'))
-    #
-    # basename = os.path.base_name
-
     print(request.url)
 
     print(path, filename)
 
-    return send_file('/home/ej/byeon.txt', as_attachment=True, mimetype='text/csv; charset=x-EBCDIC-KoreanAndKoreanExtended'
+    return send_file('/home/ej/tmp.txt', as_attachment=True, mimetype='text/csv; charset=x-EBCDIC-KoreanAndKoreanExtended'
 )
 
 if __name__=='__main__':
